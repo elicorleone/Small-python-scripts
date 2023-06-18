58322e13d6a50e1534f721327e66d8df622fef53
elicorleone <136883271+elicorleone@users.noreply.github.com>
2023-06-18 15:38:08 +0200

    Update voice_recorder.py

--- voice_recorder.py
+++ voice_recorder.py
@@ -7,3 +7,6 @@ record_voice = sounddevice.rec(int(seconds * fs), samplerate=fs, channels=2)
 sounddevice.wait()
 write("out.wav", fs, record_voice)
 
+'''Audio Recording Script using Python and sounddevice": 
+A Python script that uses the sounddevice library to record audio 
+for a specified duration and saves it as a high-quality WAV file.'''
