b628bc8f2f0c6d6aca20d45a03d3c78c48365c7a
elicorleone <136883271+elicorleone@users.noreply.github.com>
2023-06-20 18:42:30 +0200

    Update password_protector_for_pdf.py

--- password_protector_for_pdf.py
+++ password_protector_for_pdf.py
@@ -1,13 +1,15 @@
 from PyPDF2 import PdfReader, PdfWriter
 
 pdf_writer = PdfWriter()
-pdf_reader = PdfReader(r"C:\Users\Anders Falck\Desktop\Personbevis_Elis.pdf")
+pdf_reader = PdfReader(r"original.pdf") #the path of the original pdf
 
 for page in pdf_reader.pages:
     pdf_writer.add_page(page)
 
-password = "pepe@1234"
+password = "kjlakhsjkhde@1234"
 pdf_writer.encrypt(user_password=password, owner_password=None, use_128bit=True)
 
-with open('copy.pdf', 'wb') as f:
+with open('copy.pdf', 'wb') as f: #creating copy of the original pdf
     pdf_writer.write(f)
+
+   #Python script to copy a pdf file and encrypt it with password 
